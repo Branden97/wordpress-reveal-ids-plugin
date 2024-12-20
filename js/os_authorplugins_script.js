67077ab3886ce7d0d42128a28bdfd4c2e828a7a9
Branden Williams <1894876+Branden97@users.noreply.github.com>
2024-12-20 17:50:06 -0500

    feat: Sanitize/escape external data to prevent potential XSS

--- js/os_authorplugins_script.js
+++ js/os_authorplugins_script.js
@@ -1,15 +1,28 @@
 jQuery(document).ready(function() {
-jQuery("#authorplugins-start").click(function() {
-	jQuery("#authorplugins-wrap").hide();
-	jQuery.ajax({ 
-  		dataType: 'jsonp',
-  		jsonp: 'jsonp_callback',
-  		url: window.location.protocol + '//www.schloebe.de/api_portfolio.php?cat=wordpress',
-  		success: function (j) {
-			jQuery.each(j.plugins, function(i,plugin) {
-				jQuery('#authorpluginsul').append( '<li><a href="' + plugin.os_script_info_url + '" target="_blank"><span class="post">' + plugin.os_script_title + '</span><span class="hidden"> - </span><cite>version ' + plugin.os_script_version + '</cite></a></li>' ).css("display", "none").fadeIn("slow");
-    		});
-		}
-	});
-});
+    jQuery("#authorplugins-start").click(function() {
+        jQuery("#authorplugins-wrap").hide();
+        jQuery.ajax({ 
+            dataType: 'jsonp',
+            jsonp: 'jsonp_callback',
+            url: window.location.protocol + '//www.schloebe.de/api_portfolio.php?cat=wordpress',
+            success: function(j) {
+                jQuery.each(j.plugins, function(i, plugin) {
+                    // Sanitize/escape the data
+                    const sanitizedUrl = encodeURI(plugin.os_script_info_url);
+                    const sanitizedTitle = jQuery('<div>').text(plugin.os_script_title).html();
+                    const sanitizedVersion = jQuery('<div>').text(plugin.os_script_version).html();
+
+                    jQuery('#authorpluginsul').append(
+                        `<li>
+                            <a href="${sanitizedUrl}" target="_blank">
+                                <span class="post">${sanitizedTitle}</span>
+                                <span class="hidden"> - </span>
+                                <cite>version ${sanitizedVersion}</cite>
+                            </a>
+                        </li>`
+                    ).css("display", "none").fadeIn("slow");
+                });
+            }
+        });
+    });
 });
